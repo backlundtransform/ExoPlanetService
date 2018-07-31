143e18a9459fbf3e69bb7e71b8e182ea40ac20da
gbanm <gbanm06@gmail.com>
2018-07-31 23:18:19 +0200

    mapcontroller

---
+++ ExoPlanetHunterWeb/Controllers/MapsController.cs
@@ -0,0 +1,36 @@
+using ExoPlanetHunter.Service.Dto;
+using ExoPlanetHunter.Service.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+using GeoJSON.Net.Feature;
+namespace ExoPlanetHunter.Web.Controllers
+{
+    
+    [Produces("application/json")]
+    [Route("api/Maps")]
+    public class MapsController : Controller
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(typeof(ConstellationsController));
+       
+
+        [HttpGet]
+        public FeatureCollection LineString()
+        {
+            
+            try
+            {
+               string json = System.IO.File.ReadAllText("constellations.json");
+               var lines  = JsonConvert.DeserializeObject<FeatureCollection>(json);
+                return lines;
+            }
+            catch (Exception e)
+            {
+                log.Info(e.Message);
+                return null;
+            }
+        }
+    }     
+}
