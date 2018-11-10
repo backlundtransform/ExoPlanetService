352d68b657dcc59ac82a4872b0f90aba31dca3b6
gbanm <gbanm06@gmail.com>
2018-11-10 12:44:18 +0100

    ckeditor settings

--- ExoPlanetHunterWeb/ClientApp/src/components/Simulator.tsx
+++ ExoPlanetHunterWeb/ClientApp/src/components/Simulator.tsx
@@ -1,7 +1,18 @@
 import * as React from 'react';
 import { Container } from 'semantic-ui-react'
-export class Simulator extends  React.Component {
+import{Planet, Star, GetPlanetAsync,} from '../service/getPlanets'
+interface SimulatorState{x:number,y:number, alpha:number, star:Star,loading:boolean}
+interface SimulatorProps{location:any}
+export class Simulator extends  React.Component<SimulatorProps,SimulatorState> {
+state = {
+      x:0,
+      y:0,
+      alpha:0,
+      star:{} as Star,
+     loading:true
+}
  
+
     async componentDidMount(){
         }
       
