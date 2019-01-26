ad54d5d1104d8584c49f311180b433fbb58202ac
gbanm <gbanm06@gmail.com>
2019-01-27 00:45:08 +0100

    new diagram

--- ExoPlanetHunterWeb/ClientApp/src/components/Catalog.tsx
+++ ExoPlanetHunterWeb/ClientApp/src/components/Catalog.tsx
@@ -8,9 +8,9 @@ import {
   Input,
   Dropdown,
   Icon,
-  Loader, Dimmer
+  Loader,
+  Dimmer
 } from 'semantic-ui-react'
-
 import Paginate from '../common/paginate'
 import Svg, { Circle, G, ClipPath, Image, Defs } from 'react-native-svg-web'
 import MaterialIcon from 'material-icons-react'
@@ -52,18 +52,16 @@ export default class Catalog extends React.Component<any, any> {
       searchValue: '',
       planets: [] as Array<Planet>
     }
- 
   }
-  _isMounted = false;
+  _isMounted = false
   async componentDidMount() {
-    this._isMounted = true;
+    this._isMounted = true
 
     const planets = await GetPlanetListAsync(null, {}, 30)
-    this._isMounted&&this.setState({ planets, loading: false })
-
+    this._isMounted && this.setState({ planets, loading: false })
   }
-  componentWillUnmount(){
-    this._isMounted = false;
+  componentWillUnmount() {
+    this._isMounted = false
   }
 
   mainPost = () => {
@@ -71,94 +69,97 @@ export default class Catalog extends React.Component<any, any> {
     let posts = [] as Array<any>
 
     for (let item of planets as Array<Planet>) {
-      posts.push(<React.Fragment key ={item.name}>
-        <Grid.Column key={item.name}>
-          <Card className={'post-preview'} >
-            <Link
-              to={{
-                pathname: `/planet/${item.name}`,
-                state: { planet: item }
-              }}
-            >
-              <Svg height="190" width="180" >
-                {' '}
-                {Gradient()}
-                <G>
-                  <Defs>
-                    <ClipPath id="clip">
-                      <Circle cx="100" cy="100" r="65" />
-                    </ClipPath>
-                  </Defs>
-                  <Image
-                    width="180"
-                    height="190"
-                    href={`../img/${item.img.uri}.jpg`}
-                    clipPath="url(#clip)"
-                  />
-                  <Circle
-                    cx="100"
-                    cy="100"
-                    r="65"
-                    fillOpacity={0.4}
-                    fill={`url(#${item.img.uri}`}
-                  />
-                </G>
-              </Svg>{' '}
-            </Link>
-            <Card.Content>
-              <Card.Header>
-                <Link
-                  to={{
-                    pathname: `/planet/${item.name}`,
-                    state: { planet: item }
-                  }}
-                >
-                  {item.name}
-                </Link>
-              </Card.Header>
-              <Card.Description>
-                <span>{`Discovered: ${item.discYear}`}</span>
-              </Card.Description>
-              <Card.Description>
-                {`${item.type && item.type !== null ? item.type : ''}`}{' '}
-                {item.distance !== 0
-                  ? `${Math.round(item.distance)} lightyears from earth.`
-                  : ''}
-              </Card.Description>
-              <Rating
-                icon="star"
-                defaultRating={Math.round(item.esi * 10)}
-                maxRating={10}
-                size="large"
-                disabled={true}
-              />
-            </Card.Content>
-            <Card.Content extra>
+      posts.push(
+        <React.Fragment key={item.name}>
+          <Grid.Column key={item.name}>
+            <Card className={'post-preview'}>
               <Link
                 to={{
-                  pathname: `/star/${item.star.name}`,
-                  state: { star: item.star }
+                  pathname: `/planet/${item.name}`,
+                  state: { planet: item }
                 }}
               >
-                <Button icon inverted basic color="grey" height="25">
-                  <MaterialIcon icon="wb_sunny" color="#c6d4ff" size={25} />
-                  {'Visit Star'}{' '}
-                </Button>
+                <Svg height="190" width="180">
+                  {' '}
+                  {Gradient()}
+                  <G>
+                    <Defs>
+                      <ClipPath id="clip">
+                        <Circle cx="100" cy="100" r="65" />
+                      </ClipPath>
+                    </Defs>
+                    <Image
+                      width="180"
+                      height="190"
+                      href={`../img/${item.img.uri}.jpg`}
+                      clipPath="url(#clip)"
+                    />
+                    <Circle
+                      cx="100"
+                      cy="100"
+                      r="65"
+                      fillOpacity={0.4}
+                      fill={`url(#${item.img.uri}`}
+                    />
+                  </G>
+                </Svg>{' '}
               </Link>
+              <Card.Content>
+                <Card.Header>
+                  <Link
+                    to={{
+                      pathname: `/planet/${item.name}`,
+                      state: { planet: item }
+                    }}
+                  >
+                    {item.name}
+                  </Link>
+                </Card.Header>
+                <Card.Description>
+                  <span>{`Discovered: ${item.discYear}`}</span>
+                </Card.Description>
+                <Card.Description>
+                  {`${item.type && item.type !== null ? item.type : ''}`}{' '}
+                  {item.distance !== 0
+                    ? `${Math.round(item.distance)} lightyears from earth.`
+                    : ''}
+                </Card.Description>
+                <Rating
+                  icon="star"
+                  defaultRating={Math.round(item.esi * 10)}
+                  maxRating={10}
+                  size="large"
+                  disabled={true}
+                />
+              </Card.Content>
+              <Card.Content extra>
+                <Link
+                  to={{
+                    pathname: `/star/${item.star.name}`,
+                    state: { star: item.star }
+                  }}
+                >
+                  <Button icon inverted basic color="grey" height="25">
+                    <MaterialIcon icon="wb_sunny" color="#c6d4ff" size={25} />
+                    {'Visit Star'}{' '}
+                  </Button>
+                </Link>
 
-              <Link
-                to={`/system/${item.star.name}`}
-  
-              >
-                <Button icon inverted basic color="grey" height="25">
-                  <MaterialIcon icon="3d_rotation" color="#c6d4ff" size={25} />
+                <Link to={`/system/${item.star.name}`}>
+                  <Button icon inverted basic color="grey" height="25">
+                    <MaterialIcon
+                      icon="3d_rotation"
+                      color="#c6d4ff"
+                      size={25}
+                    />
 
-                  {`${item.star.noPlanets} Planets`}
-                </Button>
-              </Link>
-            </Card.Content>
-          </Card>
-        </Grid.Column></React.Fragment>
+                    {`${item.star.noPlanets} Planets`}
+                  </Button>
+                </Link>
+              </Card.Content>
+            </Card>
+          </Grid.Column>
+        </React.Fragment>
       )
     }
 
@@ -169,16 +170,15 @@ export default class Catalog extends React.Component<any, any> {
     let { searchValue } = this.state
 
     searchValue = e.target.value
-    
+
     this.setState({ searchValue })
   }
   handleSearchClick = async () => {
-   
     let filter = this.setSearchFilter()
 
     const planets = await GetPlanetListAsync(filter, {}, 30)
 
-    this._isMounted&&this.setState({ planets, loading: false })
+    this._isMounted && this.setState({ planets, loading: false })
   }
 
   handleHabClick = async () => {
@@ -191,15 +191,14 @@ export default class Catalog extends React.Component<any, any> {
     if (!habactive) {
       planets = await GetPlanetListAsync({}, {}, 30)
     }
-    this._isMounted&&this.setState({ planets, loading: false, habactive })
+    this._isMounted && this.setState({ planets, loading: false, habactive })
   }
   handlePaginate = async (top: number) => {
-
     let filter = this.setSearchFilter()
 
     const planets = await GetPlanetListAsync(filter, {}, top)
 
-    this._isMounted&&this.setState({ planets, loading: false, top })
+    this._isMounted && this.setState({ planets, loading: false, top })
   }
 
   setSearchFilter = () => {
@@ -220,44 +219,49 @@ export default class Catalog extends React.Component<any, any> {
     const { loading, searchValue, top, planets } = this.state
     const main = this.mainPost()
 
-    return loading? (
+    return loading ? (
       <Dimmer active>
-      <Loader />
+        <Loader />
       </Dimmer>
-
     ) : (
       <React.Fragment>
-             <div className={'float-left'} style={{marginLeft:20}}>
-         <Dropdown
-                className={'float-left'}
-                button
-                basic
-                floating
-                onChange={() => this.handleHabClick()}
-                options={options}
-                defaultValue="all"
-              />
-              </div>
+        <div className={'float-left'} style={{ marginLeft: 20 }}>
+          <Dropdown
+            className={'float-left'}
+            button
+            basic
+            floating
+            onChange={() => this.handleHabClick()}
+            options={options}
+            defaultValue="all"
+          />
+        </div>
         <div className={'bar'}>
-   
-        <Input type='text'
-           size="small"
-           value={searchValue}
-           onChange={(e:any) => this.handleSearchChange(e)}
-        placeholder='Search...' action>
-    <input />
-    <Button type='submit' onClick={() => this.handleSearchClick()}><Icon name={"search"} /></Button>
-    
-  </Input>
-
-   
-         </div>
-         <div className={"catalog"}>
-    <Grid stackable centered columns={2}>
-          {main}
-        </Grid>  </div>
+          <Input
+            type="text"
+            size="small"
+            value={searchValue}
+            onChange={(e: any) => this.handleSearchChange(e)}
+            placeholder="Search..."
+            action
+          >
+            <input />
+            <Button type="submit" onClick={() => this.handleSearchClick()}>
+              <Icon name={'search'} />
+            </Button>
+          </Input>
+        </div>
+        <div className={'catalog'}>
+          <Grid stackable centered columns={2}>
+            {main}
+          </Grid>{' '}
+        </div>
         <div className={'bar'}>
-        <Paginate handlePaginate={this.handlePaginate} top={top} length={planets.length} />
+          <Paginate
+            handlePaginate={this.handlePaginate}
+            top={top}
+            length={planets.length}
+          />
         </div>
       </React.Fragment>
     )
