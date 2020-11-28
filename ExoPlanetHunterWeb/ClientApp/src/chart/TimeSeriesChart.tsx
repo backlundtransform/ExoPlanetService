a43dec21041cea32098b4511c9e7bd4bf424877e
gbanm <gbanm06@gmail.com>
2020-11-28 23:45:30 +0100

    Timeseries chart

--- ExoPlanetHunterWeb/ClientApp/src/chart/TimeSeriesChart.tsx
+++ ExoPlanetHunterWeb/ClientApp/src/chart/TimeSeriesChart.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import {useState, useEffect} from 'react'
+import {useState, useEffect,useRef} from 'react'
 
   import * as am4core from "@amcharts/amcharts4/core";
 import * as am4charts from "@amcharts/amcharts4/charts";
@@ -8,40 +8,28 @@ import am4themes_animated from "@amcharts/amcharts4/themes/animated";
 import {GetTransitAsync  } from '../service/getMast'
 
 const TimeSeriesChart=({ra,dec,radius}:{ra:number,  dec:number, radius:number}): JSX.Element=> {
+
+  const chart = useRef<null | am4charts.XYChart>(null)
   const [data, setData]= useState([])
   useEffect(()=>{
   GetTransitAsync(ra,dec, radius).then(setData)
   
   },[])
  useEffect(()=>{
-   if(data.length===1){
-      return
-   }
+ 
     am4core.useTheme(am4themes_animated);
 
   
     let chart = am4core.create("timeseriesdiv", am4charts.XYChart);
+      
+    chart.data = [];
     
- 
-    let value = 50;
-    for(var i = 0; i < 300; i++){
-      let date = new Date();
-      date.setHours(0,0,0,0);
-      date.setDate(i);
-      value -= Math.round((Math.random() < 0.5 ? 1 : -1) * Math.random() * 10);
-      data.push({date:date, value: value});
-    }
-    
-    chart.data = data;
-    
-    let dateAxis = chart.xAxes.push(new am4charts.DateAxis());
+    let dateAxis = chart.xAxes.push(new am4charts.CategoryAxis());
     dateAxis.renderer.minGridDistance = 60;
-    
-    let valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
-    
+  
     let series = chart.series.push(new am4charts.LineSeries());
     series.dataFields.valueY = "value";
-    series.dataFields.dateX = "date";
+    series.dataFields.categoryX = "index";
     series.tooltipText = "{value}"
     
     series.tooltip.pointerOrientation = "vertical";
@@ -51,6 +39,14 @@ const TimeSeriesChart=({ra,dec,radius}:{ra:number,  dec:number, radius:number}):
     chart.cursor.xAxis = dateAxis;
   
  },[])
+
+ useEffect(()=>{
+ 
+  if (chart && chart.current) {
+    chart.current.data = data
+}
+
+},[data])
 
 return <div id ={'timeseriesdiv'}   style={{
     width: '100%',
