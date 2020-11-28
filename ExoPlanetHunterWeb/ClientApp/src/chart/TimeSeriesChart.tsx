e8f2fb99c8bc6488d15aba255cd53ea26c8adbfd
gbanm <gbanm06@gmail.com>
2020-11-29 00:52:56 +0100

    working chart

--- ExoPlanetHunterWeb/ClientApp/src/chart/TimeSeriesChart.tsx
+++ ExoPlanetHunterWeb/ClientApp/src/chart/TimeSeriesChart.tsx
@@ -1,49 +1,76 @@
 import * as React from 'react'
-import {useState, useEffect,useRef} from 'react'
+import {useState,   useLayoutEffect,useRef} from 'react'
 
   import * as am4core from "@amcharts/amcharts4/core";
 import * as am4charts from "@amcharts/amcharts4/charts";
 import am4themes_animated from "@amcharts/amcharts4/themes/animated";
 
-import {GetTransitAsync  } from '../service/getMast'
+import { GetTransitAsync  } from '../service/getMast'
+
+const groupBy = (items:TransitData[], key:string) => items.reduce(
+  (result, item) => ({
+    ...result,
+    [item[key]]: [
+      ...(result[item[key]] || []),
+      item,
+    ],
+  }), 
+  {},
+);
+
+interface TransitData { index:number, value:number, label:string}
 
 const TimeSeriesChart=({ra,dec,radius}:{ra:number,  dec:number, radius:number}): JSX.Element=> {
 
   const chart = useRef<null | am4charts.XYChart>(null)
-  const [data, setData]= useState([])
-  useEffect(()=>{
-  GetTransitAsync(ra,dec, radius).then(setData)
+  const [data, setData]= useState<TransitData[]>([])
+  useLayoutEffect(()=>{
+
+     GetTransitAsync(ra,dec, radius).then(setData)
   
   },[])
- useEffect(()=>{
+  useLayoutEffect(()=>{
  
-    am4core.useTheme(am4themes_animated);
+    am4core.useTheme(am4themes_animated)
+    let x= am4core.create("timeseriesdiv", am4charts.XYChart)     
+    x.data = [] 
 
+    let  categoryAxis = x.xAxes.push(new am4charts.CategoryAxis())
+    categoryAxis.renderer.minGridDistance = 60
+    categoryAxis.dataFields.category = 'index'
+    let valueAxis = x.yAxes.push(new am4charts.ValueAxis())
+    valueAxis.dataFields.data = 'value'
+
+    
   
-    let chart = am4core.create("timeseriesdiv", am4charts.XYChart);
-      
-    chart.data = [];
     
-    let dateAxis = chart.xAxes.push(new am4charts.CategoryAxis());
-    dateAxis.renderer.minGridDistance = 60;
-  
-    let series = chart.series.push(new am4charts.LineSeries());
-    series.dataFields.valueY = "value";
-    series.dataFields.categoryX = "index";
-    series.tooltipText = "{value}"
-    
-    series.tooltip.pointerOrientation = "vertical";
-    
-    chart.cursor = new am4charts.XYCursor();
-    chart.cursor.snapToSeries = series;
-    chart.cursor.xAxis = dateAxis;
+    x.cursor = new am4charts.XYCursor()
+    //x.cursor.snapToSeries = series
+    x.cursor.xAxis =categoryAxis 
+    chart.current =x
+    return () => {
+      x.dispose()
+    }
   
  },[])
 
- useEffect(()=>{
+ useLayoutEffect(()=>{
  
   if (chart && chart.current) {
-    chart.current.data = data
+   let groupedData = groupBy(data, "label")
+   console.log(groupedData )
+
+ for(const item of Object.keys(groupedData)){
+   let series = chart.current.series.push(new am4charts.LineSeries())
+    series.dataFields.valueY = item
+    series.dataFields.categoryX = 'index'
+    series.tooltipText =item 
+    series.tooltip.pointerOrientation = 'vertical'
+}
+
+    chart.current.data = data.map(p=>{
+      return { [p.label]: p.value, index:p.index}
+    })
 }
 
 },[data])
